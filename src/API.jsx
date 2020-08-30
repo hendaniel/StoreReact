024dd515edac73aedf8a1457ff5d486b03abf691
Daniel Martinez <danielmartinezdm@hotmail.com>
2020-08-29 21:32:25 -0500

    getHistory del API

--- src/API.jsx
+++ src/API.jsx
@@ -1,2 +1,3 @@
 export const PRODUCTS = "https://coding-challenge-api.aerolab.co/products";
 export const USER = "https://coding-challenge-api.aerolab.co/user/me";
+export const HISTORY = "https://coding-challenge-api.aerolab.co/user/history";
