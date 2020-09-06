d1751a87125b8e9861f0cda81a0cac7c7a38fd7e
Daniel Martinez <danielmartinezdm@hotmail.com>
2020-09-05 20:47:25 -0500

    Se quitan librerias de redux

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,5 @@ import React from "react";
 import { render } from "react-dom";
 import "./index.scss";
 import App from "./components/App";
-import { Provider } from "react-redux";
-import storeFactory from "./store";
 
-const store = storeFactory();
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+render(<App />, document.getElementById("root"));
