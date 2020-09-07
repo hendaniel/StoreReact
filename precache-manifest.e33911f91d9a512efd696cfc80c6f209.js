c33cd0bd2a76a8e50014c195e66e1ba5dfb98674
Daniel Martinez <danielmartinezdm@hotmail.com>
2020-09-06 23:58:31 -0500

    Updates

---
+++ precache-manifest.e33911f91d9a512efd696cfc80c6f209.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c76cf9e4029b01861c8fbb325c7cecd",
+    "url": "/StoreAppReact/index.html"
+  },
+  {
+    "revision": "8a2162ecbe16314cdf42",
+    "url": "/StoreAppReact/static/css/main.8fcccadc.chunk.css"
+  },
+  {
+    "revision": "a0e7ab6660cafbf14373",
+    "url": "/StoreAppReact/static/js/2.48506685.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/StoreAppReact/static/js/2.48506685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a2162ecbe16314cdf42",
+    "url": "/StoreAppReact/static/js/main.63053481.chunk.js"
+  },
+  {
+    "revision": "67625284320c149fedbb",
+    "url": "/StoreAppReact/static/js/runtime-main.194f13c6.js"
+  },
+  {
+    "revision": "2f11a957b72ff4a5c46227027cf46115",
+    "url": "/StoreAppReact/static/media/buy-blue.2f11a957.svg"
+  },
+  {
+    "revision": "004ed1f57234e1c1bc61dd79e4664229",
+    "url": "/StoreAppReact/static/media/coin.004ed1f5.gif"
+  },
+  {
+    "revision": "4669120cae147258c9fc868719b1d3ca",
+    "url": "/StoreAppReact/static/media/coin.4669120c.svg"
+  },
+  {
+    "revision": "6f48e8b94e1f23a66fcfed57b650951e",
+    "url": "/StoreAppReact/static/media/success.6f48e8b9.gif"
+  }
+]);
