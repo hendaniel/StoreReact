0f74a9d1c26f0ce663bae0ab400e2f1d73a89ea3
Daniel Martinez <danielmartinezdm@hotmail.com>
2020-09-06 20:20:59 -0500

    Se usa modal de coins en userPanel

--- src/components/User/UserPanel.jsx
+++ src/components/User/UserPanel.jsx
@@ -1,24 +1,38 @@
 import React, { useEffect, useContext } from "react";
 import { coin } from "../../assets/index";
 import { UserContext } from "../../providers/UserProvider";
+import { CoinsModal } from "../Modals/index";
+import { useModal } from "../../hooks/index";
+
 import "./user.scss";
 
 const UserPanel = () => {
   const { user } = useContext(UserContext);
 
+  const [coinsModal, toggleCoinsModal] = useModal();
+
   useEffect(() => {
     if (!user) return;
   }, [user]);
 
+  const openCoinsModal = () => {
+    toggleCoinsModal();
+  };
+
   const { name, points } = user;
   return (
-    <div className="user-panel">
-      <div>{name}</div>
-      <div className="coins-user">
-        <img src={coin} alt="Coins" />
-        <span className="points">{points}</span>
+    <>
+      {coinsModal && (
+        <CoinsModal isShowing={coinsModal} hide={toggleCoinsModal} />
+      )}
+      <div className="user-panel">
+        <div>{name}</div>
+        <div className="coins-user">
+          <img src={coin} onClick={openCoinsModal} alt="Coins" />
+          <span className="points">{points}</span>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default UserPanel;
